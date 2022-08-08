4b4d90b040a953886838846d865cd550d95bd12e
faliwlove <allinurmagribi@gmail.com>
2022-08-08 15:13:39 +0700

    Folder Incubator

---
+++ Incubator_Code/incubator_code.ino
@@ -0,0 +1,27 @@
+#define pin_SHT15 1
+#define pin_babyTemp_1 2
+#define pin_babyTemp_2 3
+
+// #include "src/folder library"
+
+// Variabel
+
+
+void main(){
+    // inisialisai, pin
+
+}
+void loop(){
+
+}
+
+void baca_suhu_airway(){
+
+}
+void control_suhu_airway(){
+
+}
+void tampil_suhu_airway(){
+
+}
+
